99cd725cfd35a2049a85278bbbd7fd4964d6050b
Laure Fischer <laurefischer27@gmail.com>
2014-03-24 11:23:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Surepack::Application.config.secret_key_base = '49d551026619d9523b67ebcf05861845558e4de2ff226834b15b72f1bb8a2d19bbd4d1c3bc6f4bd61f45737a2bd4f8510cd5550525302c5bf3e04e16f02a1d34'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Surepack::Application.config.session_store :cookie_store, key: '_surepack_session'
