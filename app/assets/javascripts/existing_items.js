b1814f7bc880a69e092a893dea0b3696856678c5
Laure Fischer <laurefischer27@gmail.com>
2014-03-25 15:38:28 -0400

    added functionality to show all items when user clicks add item from existing

--- app/assets/javascripts/existing_items.js
+++ app/assets/javascripts/existing_items.js
@@ -6,9 +6,11 @@ $(document).ready(function(){
   })
   .done(function(data) {
     Master.storeData(data);
+    console.log("Done")
   });
 
-  // $('.add-existing').click(Master.renderMasterList);
+  $('#add-existing').click(Master.renderMasterList);
+  // $('.add-button').click(Master.addToList);
 
 });
 
@@ -21,10 +23,36 @@ Master.storeData = function(data) {
 };
 
 Master.renderMasterList = function(event) {
-  var masterHTML;
-  masterHTML = Master.renderMasterHTML();
+  event.preventDefault();
+  var items = Master.items;
+  Master.renderMasterHTML(items);
 };
 
-Master.addToList = function(event) {
-
+Master.renderMasterHTML = function(items) {
+  var i = 0;
+  for (; i < Master.items.length; i++) {
+    Master.renderItemHTML(items[i]);
+  }
 };
+
+Master.renderItemHTML = function(item) {
+  var $itemDiv = '<div>' + item.item_name + '<button class="add-button">+ Add</button></div>';
+  $('#existing-list').append($itemDiv);
+  $('#new-list').hide("slow");
+};
+
+// Master.addToList = function(event) {
+//   event.preventDefault();
+
+//   $.ajax({
+//     type: 'POST',
+//     url: '/lists/' + list_id + '/items',
+//     data: { list_id: list_id, user_id: user_id }
+//   })
+//   .done(function(data){
+//     var itemHTML = '<li>' +  + '</li>';
+//     $('#new-list').append(itemHTML);
+//   });
+
+//   return false;
+// };
