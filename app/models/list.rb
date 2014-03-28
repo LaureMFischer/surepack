022e6df17fae257ea96f5f682d4a649c1bb7ec54
Laure Fischer <laurefischer27@gmail.com>
2014-03-27 20:46:07 -0400

    added model method to parse date of trip for a list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,13 @@ class List < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :items
   validates_uniqueness_of :name, scope: :user_id
+
+  def parse_date
+    months = { 1 => "Jan", 2 => "Feb", 3 => "Mar", 4 => "Apr", 5 => "May", 6 => "Jun", 7 => "Jul", 8 => "Aug", 9 => "Sep", 10 => "Oct", 11 => "Nov", 12 => "Dec" }
+    month_number = Date._parse(self.deadline_date.to_s)[:mon]
+    day = Date._parse(self.deadline_date.to_s)[:mday].to_s
+    year = Date._parse(self.deadline_date.to_s)[:year].to_s
+    month = months[month_number]
+    month + " " + day + ", " + year
+  end
 end
